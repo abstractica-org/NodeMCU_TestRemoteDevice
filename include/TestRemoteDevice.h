f25cf578c5e0c46fa0ec3daccf615874c70862c4
tgrundtvig <TGrundtvig@gmail.com>
2025-01-28 17:53:25 +0100

    Updated to new blinker protocol

--- include/TestRemoteDevice.h
+++ include/TestRemoteDevice.h
@@ -5,6 +5,10 @@
 #include <RemoteDevice.h>
 #include <LEDBlinker.h>
 
+#define CMD_START_BLINK 1000
+#define CMD_BLINK_STOPPED 2000
+
+
 
 class TestRemoteDevice : public RemoteDevice
 {
@@ -14,6 +18,9 @@ public:
     virtual uint16_t onPacketReceived(uint16_t command, uint16_t arg1, uint16_t arg2, uint16_t arg3, uint16_t arg4, uint8_t* pData, uint16_t size);
 private:
     LEDBlinker _blinker;
+    unsigned long _curTime;
+    unsigned long _BlinkerEndTime;
+    bool _stopSend;
 };
 
 #endif
--- src/TestRemoteDevice.cpp
+++ src/TestRemoteDevice.cpp
@@ -3,26 +3,34 @@
 
 TestRemoteDevice::TestRemoteDevice() : RemoteDevice(ESP.getChipId(), "TestRemoteDevice", 1), _blinker(2, false, 200, 400, 2000)
 {
-
+    _curTime = 0;
+    _BlinkerEndTime = 0;
+    _stopSend = true;
 };
 
 void TestRemoteDevice::update(unsigned long curTimeMS)
 {
+    _curTime = curTimeMS;
     RemoteDevice::update(curTimeMS);
     _blinker.update(curTimeMS);
+    if(!_stopSend)
+    {
+        _stopSend = sendPacketToServer(CMD_BLINK_STOPPED, 1, 2, 3, 4);
+    }
+    if(_blinker.isBlinking() && _curTime > _BlinkerEndTime)
+    {
+        _blinker.stopBlink();
+        _stopSend = sendPacketToServer(CMD_BLINK_STOPPED, 1, 2, 3, 4);
+    }
 };
 
 uint16_t TestRemoteDevice::onPacketReceived(uint16_t command, uint16_t arg1, uint16_t arg2, uint16_t arg3, uint16_t arg4, uint8_t* pData, uint16_t size)
 {
-    if(command == 0)
-    {
-        _blinker.stopBlink();
-        return 1;
-    }
-    else if(command == 1)
+    if(command == CMD_START_BLINK)
     {
         _blinker.startBlink(arg1);
-        return 1;
+        _BlinkerEndTime = _curTime + arg2*1000;
+        return 0;
     }
     else
     {
